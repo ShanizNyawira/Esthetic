1b754cee16532dac91008d0fda98d5bb3e185726
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-10-07 01:05:46 +0300

    Added Likes functionality

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,15 +1,33 @@
-import {AiOutlineHeart} from 'react-icons/ai';
+import {AiFillHeart} from 'react-icons/ai';
 import {useState} from "react"
 
 function Art({ art }) {
-    const [liked,setLiked] = useState()
+    const [liked,setLiked] = useState(art.likes>0)
+    const [noOfLikes,setNoOfLikes] = useState(art.likes)
     return (
         <div className="art">
             <img src={art.image} alt="art" />
             <p>{art.artist}</p>
             <div className="likes">
-                <span>{art.likes}</span>
-                <AiOutlineHeart onClick={()=>console.log("clicked")}/>
+                <span>{noOfLikes}</span>
+                <AiFillHeart 
+                style={{color: liked ? "red" : "black"}}
+                onClick={()=>{
+                    setLiked(!liked)
+                    
+                    //increase likes
+                    if(!liked){
+                        setNoOfLikes(noOfLikes+1)
+                    }
+                    //decrease likes
+                    else{
+                        setNoOfLikes(noOfLikes-1)
+                    }
+
+                }}
+                
+                    
+                    />
             </div>
         </div>
 
