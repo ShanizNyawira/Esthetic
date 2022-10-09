48334eaeb7086c6456ce568cfbc6bc6f41ec214d
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-10-09 11:40:03 +0300

    Add styling for body

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,33 +1,35 @@
-import {AiFillHeart} from 'react-icons/ai';
-import {useState} from "react"
+import { AiFillHeart } from 'react-icons/ai';
+import { useState } from "react"
 
 function Art({ art }) {
-    const [liked,setLiked] = useState(art.likes>0)
-    const [noOfLikes,setNoOfLikes] = useState(art.likes)
+    const [liked, setLiked] = useState(art.likes > 0)
+    const [noOfLikes, setNoOfLikes] = useState(art.likes)
     return (
         <div className="art">
-            <img src={art.image} alt="art" />
+            <div className='img'>
+                <img src={art.image} alt="art" />
+            </div>
             <p>{art.artist}</p>
             <div className="likes">
                 <span>{noOfLikes}</span>
                 <AiFillHeart
-                style={{color: liked ? "red" : "black"}}
-                onClick={()=>{
-                    setLiked(!liked)
+                    style={{ color: liked ? "red" : "black" }}
+                    onClick={() => {
+                        setLiked(!liked)
 
-                    //increase likes
-                    if(!liked){
-                        setNoOfLikes(parseInt(noOfLikes+1))
-                    }
-                    //decrease likes
-                    else{
-                        setNoOfLikes(parseInt(noOfLikes-1))
-                    }
+                        //increase likes
+                        if (!liked) {
+                            setNoOfLikes(parseInt(noOfLikes + 1))
+                        }
+                        //decrease likes
+                        else {
+                            setNoOfLikes(parseInt(noOfLikes - 1))
+                        }
 
-                }}
+                    }}
 
 
-                    />
+                />
             </div>
         </div>
 
